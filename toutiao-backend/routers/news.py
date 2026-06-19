ca71643dca1e70e45ca7948adeed38b6d9cbbd7a
ssk <3312336898@qq.com>
2026-06-19 18:01:16 +0800

    获取新闻分类

--- toutiao-backend/routers/news.py
+++ toutiao-backend/routers/news.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from config.db_conf import get_db 
+from crud import news
+
 
 router = APIRouter(prefix="/api/news", tags=["news"])
 
 @router.get("/categories")
-async def get_news_categories():
-    # 这里可以添加获取新闻分类的逻辑
-    return {"message": ["体育", "娱乐", "科技", "财经"]}
+async def get_news_categories(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
+    categories = await news.get_categories(db, skip, limit)
+    return {
+        "code": 200,
+        "message": "成功获取新闻分类列表",
+        "data": categories
+    }
